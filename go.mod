d6c8750987374f9a86741a888a6c7f4a042c2267
Thomas Hallgren <thomas@tada.se>
2019-05-13 17:58:37 +0200

    Initial commit

---
+++ go.mod
@@ -0,0 +1,6 @@
+module github.com/thallgren/lyra-sample
+
+require (
+	github.com/lyraproj/pcore v0.0.0-20190513110440-3136007d878d
+	github.com/lyraproj/servicesdk v0.0.0-20190513111104-a5b399ba7e1d
+)
---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"github.com/hashicorp/go-hclog"
+	"github.com/lyraproj/pcore/px"
+	"github.com/lyraproj/pcore/types"
+	"github.com/lyraproj/servicesdk/lang/go/lyra"
+)
+
+type In struct {
+	Name   string
+	Number int
+}
+
+type Out struct {
+	Hash map[string]px.Value
+}
+
+func main() {
+	// Workflow input is from Hiera and a constant (declared by annotations in the In struct).
+	lyra.Serve(`lyra_sample`, nil, &lyra.Workflow{
+		Parameters: &In{},
+		Return:     &Out{},
+		Steps: map[string]lyra.Step{
+			`start`: &lyra.Action{
+				Do: func(input struct{ Name string }) struct{ X int } {
+					hclog.Default().Info("first", "Name", input.Name)
+					return struct{ X int }{32}
+				}},
+			`end`: &lyra.Action{
+				Do: func(input struct {
+					Name   string
+					Number int
+					X      int
+				}) *Out {
+					hclog.Default().Info("first", "Name", input.Name)
+					return &Out{
+						map[string]px.Value{
+							`hey`: types.WrapString(input.Name),
+							`ho`:  types.WrapInteger(int64(input.Number + input.X))}}
+				},
+			},
+		}})
+}
